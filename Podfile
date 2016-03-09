7b62dd856592c81673d59bc6d1e7d2fccdba2ecb
On1Key <macbook@macdeMacBook-Pro.local>
2016-03-09 15:33:10 +0800

    添加GRMustache框架简单用法

--- Podfile
+++ Podfile
@@ -1,2 +1,3 @@
 platform:ios,'8.0'
 pod 'AFNetworking', '~> 3.0.4'
+pod 'GRMustache', '~> 7.3.2'
--- iOS_Pod_CodeDemo/ViewController.m
+++ iOS_Pod_CodeDemo/ViewController.m
@@ -11,10 +11,17 @@
 //
 //
 
-#import "ViewController.h"
-#import <AFNetworking.h>
+#pragma mark - 添加规范注释
+/**
+ *  ------------------------------------------------------------------
+ *  添加规范注释
+ */
 
-@interface ViewController ()
+#import "ViewController.h"
+
+
+
+@interface ViewController ()<UIWebViewDelegate>
 
 @end
 
@@ -23,7 +30,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    //测试GRMustache框架，使用字典改变html文件的内容
+    [self testOf_GRMustache_changeWebViewTextByTitle:@"title名称" info:@"文本内容"];
 }
+#pragma mark - GRMustache框架，html修改
+/**
+ *  ------------------------------------------------------------------
+ *  测试GRMustache框架，使用字典改变html文件的内容
+ */
+- (void)testOf_GRMustache_changeWebViewTextByTitle:(NSString *)title info:(NSString *)info{
+    //这个htmlString用来和字典改变html文件内容比较
+    //可通过打开下面的注释作对比
+    NSString *htmlString = @"<html>"
+    "<head>"
+    "<h1>title名称ss</h1>"
+    "</head>"
+    "<body>"
+    "<p>文本内容ss</p>"
+    "</body>"
+    "</html>";
+    UIWebView *webview = [[UIWebView alloc] initWithFrame:CGRectMake(50, 100, SCREEN_WIDTH - 100, 300)];
+    webview.delegate = self;
+    webview.backgroundColor = COLOR_RANDOM;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"customer" ofType:@"html"];
+    NSString *pathString = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    NSDictionary *dic = @{@"name": @"title名称",@"content":@"文本内容"};
+    NSString *content = [GRMustacheTemplate renderObject:dic fromString:pathString error:nil];
+    [self.view addSubview:webview];
+//    [webview loadHTMLString:htmlString baseURL:nil];
+    [webview loadHTMLString:content baseURL:[NSURL URLWithString:path]];
+}
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    NSLog(@"%s\n%@\n%@",__func__,request,request.URL.scheme);
+    return YES;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
